ef746d0b061dcee5a49b5616d5b7c5624849acd8
dmytro.shafieiev <shafeyevdmitriy@gmail.com>
2021-10-26 22:20:17 +0300

    Interval List Intersections
    You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
    
    Return the intersection of these two interval lists.
    
    A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
    
    The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
    
    Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
    Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]

---
+++ src/com/company/IntervalsListIntersection.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntervalsListIntersection {
+
+  public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+
+    int firstListPointer = 0;
+    int secondListPointer = 0;
+
+    List<int[]> resultList = new ArrayList<>();
+
+    while(firstListPointer < firstList.length && secondListPointer < secondList.length) {
+
+      int firstIntervalStart = firstList[firstListPointer][0];
+      int firstIntervalEnd = firstList[firstListPointer][1];
+      int secondIntervalStart = secondList[secondListPointer][0];
+      int secondIntervalEnd = secondList[secondListPointer][1];
+
+      if(firstIntervalStart <= secondIntervalEnd && firstIntervalEnd >= secondIntervalStart) {
+
+        int [] mergedInterval = new int [2];
+
+        mergedInterval[0] = Math.max(firstIntervalStart, secondIntervalStart);
+        mergedInterval[1] = Math.min(firstIntervalEnd, secondIntervalEnd);
+
+        resultList.add(mergedInterval);
+      }
+
+      if(firstList[firstListPointer][1] > secondList[secondListPointer][1] ) {
+        secondListPointer++;
+      }else {
+        firstListPointer++;
+      }
+
+    }
+
+    int [][]  resultArray = new int [resultList.size()][];
+
+    return resultList.toArray(resultArray);
+
+  }
+
+  //TC: O(n+m) SC: O(n+m)
+
+}
