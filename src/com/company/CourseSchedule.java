8463dd2cd83eef0e530d37ca68c1ef65d9ce83b9
dmytro.shafieiev <shafeyevdmitriy@gmail.com>
2021-10-29 23:18:53 +0300

    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
    Return true if you can finish all courses. Otherwise, return false.
    
    Example 1:
    
    Input: numCourses = 2, prerequisites = [[1,0]]
    Output: true
    Explanation: There are a total of 2 courses to take.
    To take course 1 you should have finished course 0. So it is possible.

---
+++ src/com/company/CourseSchedule.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CourseSchedule {
+
+  //TC: O(V*E) SC: O(V*E);
+
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    if (prerequisites.length == 0) {
+      return true;
+    }
+
+    int[] indegree = new int[numCourses];
+    Queue<Integer> zeroDegrees = new LinkedList<>();
+    Queue<Integer> result = new LinkedList<>();
+
+    countIndegreeForEachCourse(prerequisites, indegree);
+
+    addCoursesWithNoPrerToQueue(indegree, zeroDegrees);
+
+    //NO course has degrees (no connection between vertexes)
+    if (zeroDegrees.isEmpty()) {
+      return false;
+    }
+
+    //BFS across graph of courses deleting vertex with no in-degree edges and terminating connections with other vertexes
+    while (!zeroDegrees.isEmpty()) {
+      int course = zeroDegrees.poll();
+      result.add(course);
+
+      for (int[] prer : prerequisites) {
+
+        if (prer[1] == course) {
+          indegree[prer[0]]--;
+
+          if (indegree[prer[0]] == 0) {
+            zeroDegrees.add(prer[0]);
+          }
+        }
+      }
+
+
+    }
+
+    for (int elem : indegree) {
+      if (elem != 0) {
+        return false;
+      }
+    }
+
+    return true;
+
+  }
+
+
+
+  private void addCoursesWithNoPrerToQueue(int[] indegree, Queue<Integer> zeroDegrees) {
+    for (int pointer = 0; pointer < indegree.length; pointer++) {
+
+      if (indegree[pointer] == 0) {
+        zeroDegrees.add(pointer);
+      }
+
+    }
+  }
+
+  private void countIndegreeForEachCourse(int[][] prerequisites, int[] indegree) {
+    for (int[] prer : prerequisites) {
+
+      indegree[prer[0]]++;
+
+    }
+  }
+
+}
