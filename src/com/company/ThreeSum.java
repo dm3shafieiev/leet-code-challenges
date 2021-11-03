c196c8ea2fc046393cfd83242a1ff87029b41384
dmytro.shafieiev <shafeyevdmitriy@gmail.com>
2021-11-03 10:44:30 +0200

    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
    Notice that the solution set must not contain duplicate triplets.
    
    Example 1:
    
    Input: nums = [-1,0,1,2,-1,-4]
    Output: [[-1,-1,2],[-1,0,1]]

---
+++ src/com/company/ThreeSum.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ThreeSum {
+
+  //TC: O(n2) SC: O(n)
+  public List<List<Integer>> threeSum(int[] nums) {
+
+    if(nums.length == 0) {
+      return new ArrayList<>();
+    }
+
+    Map<Integer, Integer> map = new HashMap<>();
+    Set<List<Integer>> result = new HashSet<>();
+    Set<Integer> dublicates = new HashSet<>();
+
+    for(int fixedPointer = 0; fixedPointer < nums.length; fixedPointer++) {
+
+      int fixedNum = nums[fixedPointer];
+
+      if (dublicates.add(fixedNum)) {
+
+        for(int pointer = fixedPointer + 1; pointer < nums.length; pointer++) {
+
+          if(map.containsKey(nums[pointer]) && (fixedNum + nums[pointer] + map.get(nums[pointer])) == 0) {
+
+            List<Integer> triplet = Arrays.asList(fixedNum, nums[pointer], map.get(nums[pointer]));
+            Collections.sort(triplet);
+            result.add(triplet);
+          }
+
+
+          map.put(-fixedNum - nums[pointer], nums[pointer]);
+
+        }
+      }
+
+    }
+    return new ArrayList(result);
+  }
+}
