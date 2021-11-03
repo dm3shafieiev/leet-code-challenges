7a93622dbcb6ea36d878a23baccf4d7febb71321
dmytro.shafieiev <shafeyevdmitriy@gmail.com>
2021-11-03 10:44:43 +0200

    Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
    Notice that the solution set must not contain duplicate triplets.
    
    Example 1:
    
    Input: nums = [-1,0,1,2,-1,-4]
    Output: [[-1,-1,2],[-1,0,1]]

--- src/com/company/ThreeSum.java
+++ src/com/company/ThreeSum.java
@@ -1,6 +1,8 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
